2b10b6b9ad97190e8c50815b72d846a045c53c82
shuhei_hagino <shuhei_hagino@mediado.jp>
2022-04-27 11:24:38 +0900

    1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,19 @@
 class FavoritesController < ApplicationController
 
   def create
+    respond_to do |format|#
     current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    #redirect_to blogs_path
+    format.js { render :index }#
+    end#
   end
 
   def destroy
+    respond_to do |format|
     current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    #redirect_to blogs_path
+    format.js { render :index }#
+  end#
   end
 
 end
